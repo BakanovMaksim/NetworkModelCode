69383840ae97ab5e2a90e22de13acd74ba83e02f
maksim <bakanovmp12@gmail.com>
2021-01-18 16:03:22 +0300

    NetworkModelCode.Desktop: feature interface wpf

---
+++ NetworkModelCode.Desktop/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,31 @@
+﻿using NetworkModelCode.Core.Application.Calculators;
+using NetworkModelCode.Infrastructure.Business.Parsers;
+using NetworkModelCode.Desktop.Models;
+
+using System.Windows.Controls;
+
+namespace NetworkModelCode.Desktop.ViewModels
+{
+    public class ProjectViewModel
+    {
+        public Project Project { get; }
+        private MathModelFileParser Parser { get; }
+
+        public ProjectViewModel()
+        {
+            Project = new Project();
+            Parser = new MathModelFileParser();
+        }
+
+        public void CalculateMathModelTemporary(DataGrid dataGrid)
+        {
+            var isTry = Parser.TryParseSource(Project.Data, out var mathModelSource);
+
+            if(isTry)
+            {
+                var calculator = new MathModelTemporaryCalculator(mathModelSource);
+                var mathModelTemporary = calculator.Calculate();
+            }
+        }
+    }
+}
---
+++ NetworkModelCode.Desktop/Views/ProjectPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using NetworkModelCode.Desktop.ViewModels;
+
+using System.Windows.Controls;
+
+namespace NetworkModelCode.Desktop.Views
+{
+    public partial class ProjectPage : Page
+    {
+        internal ProjectViewModel ProjectViewModel { get; }
+        public ProjectPage()
+        {
+            InitializeComponent();
+
+            ProjectViewModel = new ProjectViewModel();
+            DataContext = ProjectViewModel;
+        }
+
+        private void enterButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ProjectViewModel.CalculateMathModelTemporary(tableDataGrid);
+        }
+    }
+}
