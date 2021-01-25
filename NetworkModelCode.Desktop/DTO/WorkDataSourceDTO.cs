bd00eb7c87315c190962d1bb7c72a141b8c90cd1
maksim <bakanovmp12@gmail.com>
2021-01-25 15:33:38 +0300

    NetworkModelCode.Desktop: feature a dto model and mapper

---
+++ NetworkModelCode.Desktop/DTO/WorkDataSourceDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace NetworkModelCode.Desktop.DTO
+{
+    public class WorkDataSourceDTO
+    {
+        [DisplayName("i")]
+        public int CodeI { get; set; }
+
+        [DisplayName("j")]
+        public int CodeJ { get; set; }
+
+        [DisplayName("Продолжительность")]
+        public int Time { get; set; }
+    }
+}
